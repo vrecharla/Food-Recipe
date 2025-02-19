f579284bd3bd947625132a37046c12f72821c8ba
vrecharla <vrecharla@slu.edu>
2025-02-19 14:11:42 -0600

    changes to authroute

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -37,7 +37,7 @@ router.post('/login', async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 
         res.json({ token });
     } catch (error) {
